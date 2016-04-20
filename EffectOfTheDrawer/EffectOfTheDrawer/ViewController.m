81d579fc759b2cc6eda35704c1ada407babe66e8
susu <28025920@qq.com>
2016-04-20 20:35:32 +0800

    Implementing effect of drawer.

--- EffectOfTheDrawer/EffectOfTheDrawer/ViewController.m
+++ EffectOfTheDrawer/EffectOfTheDrawer/ViewController.m
@@ -7,8 +7,13 @@
 //
 
 #import "ViewController.h"
-
+#import <CoreGraphics/CoreGraphics.h>
 @interface ViewController ()
+@property(nonatomic,strong)UIView* mainView;
+
+@property(strong,nonatomic)UIView* blueView;
+
+@property(strong,nonatomic)UIView* greenView;
 
 @end
 
@@ -16,12 +21,61 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    [self setView];
+    
+    UIPanGestureRecognizer* pan=[[UIPanGestureRecognizer alloc]initWithTarget:self action:@selector(pan:)];
+    
+    [self.view addGestureRecognizer:pan];
+    
+    //KVO
+    [self.mainView addObserver:self forKeyPath:@"frame" options:NSKeyValueObservingOptionNew  context:nil];
+    
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+//KVO方法
+-(void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary<NSString *,id> *)change context:(void *)context{
+    
+    if (self.mainView.frame.origin.x<0) {
+        self.greenView.hidden=YES;
+    }else{
+        self.greenView.hidden=NO;
+    }
+    
+}
+
+
+- (void)pan:(UIPanGestureRecognizer *)pan{
+    
+    CGPoint transP = [pan translationInView:self.view];
+    
+    CGFloat offsetX=transP.x;
+    
+    self.mainView.frame=CGRectMake(self.mainView.frame.origin.x+offsetX, 0, self.view.bounds.size.width, self.view.bounds.size.height);
+    [pan setTranslation:CGPointZero inView:self.view];
+    
+    
+}
+
+
+-(void)setView{
+    
+    
+    UIView* blueView=[[UIView alloc]initWithFrame:self.view.bounds];
+    blueView.backgroundColor=[UIColor blueColor];
+    [self.view addSubview:blueView];
+    self.blueView=blueView;
+    
+    UIView* greenView=[[UIView alloc]initWithFrame:self.view.bounds];
+    greenView.backgroundColor=[UIColor greenColor];
+    [self.view addSubview:greenView];
+    self.greenView=greenView;
+    
+    UIView* redView=[[UIView alloc]initWithFrame:self.view.bounds];
+    redView.backgroundColor=[UIColor redColor];
+    [self.view addSubview:redView];
+    self.mainView=redView;
+    
 }
 
 @end
